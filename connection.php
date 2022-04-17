1777c1b44f27375af263350dccaadb0c57518f1b
pate0845 <pate0845@algonquinlive.com>
2022-04-17 13:11:40 -0400

    env added

--- connection.php
+++ connection.php
@@ -1,9 +1,13 @@
 <?php
-$host=getenv('Port');
-$passwd=getenv('Password');
-$db=getenv('Database');
-$username=getenv('Username');
-$connection=new mysqli($host, $username, $passwd, $db);
+
+$url = getenv('JAWSDB_URL');
+$dbparts = parse_url($url);
+$hostname = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$database = ltrim($dbparts['path'],'/');
+
+$connection=new mysqli($hostname, $username, $password, $database);
 if($connection->connect_error){
     die("Unable to connect database: ".$connection->connect_error);
 }
