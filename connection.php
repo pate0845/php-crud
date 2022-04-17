0e43d9d1763e0dc8e5b0573aaa1a70e20d6d0753
pate0845 <pate0845@algonquinlive.com>
2022-04-17 12:47:15 -0400

    .added env file!

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-$host='localhost';
-$passwd='';
-$db='login';
-$username='root';
+$host=getenv('Port');
+$passwd=getenv('Password');
+$db=getenv('Database');
+$username=getenv('Username');
 $connection=new mysqli($host, $username, $passwd, $db);
 if($connection->connect_error){
     die("Unable to connect database: ".$connection->connect_error);
